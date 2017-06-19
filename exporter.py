92047e93708426125fab39698f9d4ae8de6b93fd
Ben Edwards <ben@edwards.nz>
2017-06-19 15:16:48 +1200

    Stuff

--- exporter.py
+++ exporter.py
@@ -1,5 +1,6 @@
 from xero import Xero
 from xero.auth import PrivateCredentials
+from invoices import INVOICE_IDS
 
 import csv
 
@@ -72,4 +73,76 @@ def export_credit_notes():
                 'CreditNoteNumber': invoice.get('CreditNoteNumber'),
                 'Total': invoice.get('Total')
             })
+
+
+def void_invoices(invoice_ids=None):
+    """
+    Void a list of invoices
+    """
+
+    xero = auth_xero()
+
+    # Load the defaults if they 
+    if not invoice_ids:
+        invoice_ids = INVOICE_IDS
+
+    with open('void_results.csv', 'wb') as csvfile:
+
+        fieldnames = ['InvoiceID','Current Status','Result','Error']
+
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for invoice_id in invoice_ids:
+
+            result = None
+            error = None
+
+            print '\n\nVoiding Invoice %s' % invoice_id
+
+            # Check the current status
+            xero_invoice = xero.invoices.get(invoice_id)
+
+            current_status = xero_invoice[0].get('Status')
+
+            print '    Current invoice status is %s' % current_status
+
+            if current_status != 'VOIDED':
+
+                invoice_to_void = {
+                    'InvoiceID': invoice_id,
+                    'Status': 'VOIDED',
+                    'LineAmountTypes': 'NoTax'
+                }
+
+                try:
+                    result = xero.invoices.save(invoice_to_void)
+
+                    if result[0].get('Status') == 'VOIDED':
+                        result = 'Void'
+                        print '        Successfully voided invoice'
+                    else:
+                        result = 'Failed'
+                        print '        Still not voided...'
+
+                except Exception as ex:
+                    print '        Failed to void invoice %s' % str(ex)
+                    result = 'Failed'
+                    error = str(ex)
+
+            else:
+
+                result = 'Void'
+
+                print '        Invoice already voided, doing nothing!'
+
+
+            writer.writerow({
+                'InvoiceID': invoice_id,
+                'Current Status': current_status,
+                'Result': result,
+                'Error': error
+            })
+
 
